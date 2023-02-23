635bab50ef33a5fbaea86bac0d67f077582ac8b6
Daniel R <daniel.r@automattic.com>
2023-02-22 22:19:16 -0800

    Make item-card divs clickable

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -4,7 +4,7 @@ const ItemCard = ({pairedItem}) => {
     let key = 0;
     
     return (
-        <div className='item-card' key={key++}>
+        <div className='item-card' key={key++} onClick={(e) => console.log('it worked')}>
             {pairedItem}
         </div>
     )
