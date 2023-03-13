ca5b097884410695e505de26f58641d36edb3b3f
Daniel R <daniel.r@automattic.com>
2023-03-12 18:59:53 -0700

    Added alt text to image on Error404 component

--- src/components/Error404.js
+++ src/components/Error404.js
@@ -5,7 +5,11 @@ const Error404 = () => {
   return (
     <div className="error404-div">
       <h1>Error 404</h1>
-      <img src={require("../images/logo192.png")} width="100px"></img>
+      <img
+        src={require("../images/logo192.png")}
+        alt="Truth Tally logo"
+        width="100px"
+      ></img>
       <h2>Not Found</h2>
       <NavLink to="/">Start a new list</NavLink>
     </div>
