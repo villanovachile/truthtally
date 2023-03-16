b9a0786ca7560f9e8e5c3c4c28aa04fda2c5e263
villanovachile <65082164+villanovachile@users.noreply.github.com>
2023-03-15 20:40:44 -0700

    Updated Netlify functions to use singleton connection (#12)

--- netlify/functions/get_list/get_list.js
+++ netlify/functions/get_list/get_list.js
@@ -1,44 +1,33 @@
-const { MongoClient } = require("mongodb");
-
-const mongoClient = new MongoClient(process.env.MONGODB_URI, {
-  socketTimeoutMS: 5000,
-  connectTimeoutMS: 5000,
-  serverSelectionTimeoutMS: 5000,
-});
-const clientPromise = mongoClient.connect();
+const connectToDatabase = require("../../../utils/mongo-connection");
 
 const handler = async (event) => {
   const input = event.queryStringParameters.uri;
 
   try {
-    const database = (await clientPromise).db(process.env.MONGODB_DATABASE);
+    const database = await connectToDatabase(process.env.MONGODB_URI);
     const collection = database.collection(process.env.MONGODB_COLLECTION);
     const results = await collection.find({ [input]: { $exists: true } }).toArray();
 
-    try {
-      if (results.length > 0) {
-        let viewCount = results[0].views;
-        await collection.updateOne(
-          { [input]: { $exists: true } },
-          {
-            $set: {
-              views: viewCount + 1,
-            },
-          }
-        );
+    if (results.length > 0) {
+      let viewCount = results[0].views;
+      await collection.updateOne(
+        { [input]: { $exists: true } },
+        {
+          $set: {
+            views: viewCount + 1,
+          },
+        }
+      );
 
-        return {
-          statusCode: 200,
-          body: JSON.stringify(results[0]),
-        };
-      } else {
-        return {
-          statusCode: 200,
-          body: JSON.stringify("not_found"),
-        };
-      }
-    } catch (error) {
-      return { statusCode: 500, body: error.toString() };
+      return {
+        statusCode: 200,
+        body: JSON.stringify(results[0]),
+      };
+    } else {
+      return {
+        statusCode: 200,
+        body: JSON.stringify("not_found"),
+      };
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() };
---
+++ utils/mongo-connection.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
+
+let cachedDb = null;
+
+async function connectToDatabase(uri) {
+  if (cachedDb) {
+    return cachedDb;
+  }
+
+  const client = await MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+  cachedDb = client.db(process.env.MONGODB_DATABASE);
+  return cachedDb;
+}
+
+module.exports = connectToDatabase;
