b971c353ed0e65f39a17164c17ed16693329c033
Daniel R <daniel.r@automattic.com>
2023-03-22 21:03:28 -0700

    Updated CSS for title on desktop

--- src/components/TruthTally/Item.js
+++ src/components/TruthTally/Item.js
@@ -138,6 +138,7 @@ const Item = ({
       {listState === 'edit' && (
         <div>
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+            <title>Drag list item</title>
             <path d="M2 11h16v2H2zm0-4h16v2H2zm8 11l3-3H7l3 3zm0-16L7 5h6l-3-3z" />
           </svg>
         </div>
