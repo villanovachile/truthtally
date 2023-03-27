ccecffbc1b38736613f0d74289a8f08d21e2a79b
Daniel R <daniel.r@automattic.com>
2023-03-26 22:18:54 -0700

    Refactor LoadingSpinner

--- src/components/TruthTally/LoadingSpinner.js
+++ src/components/TruthTally/LoadingSpinner.js
@@ -4,20 +4,23 @@ const LoadingSpinner = (props) => {
   const { gameState, setGameState, loadingText, rankingCompleted } = props;
   const [showSpinner, setShowSpinner] = useState(false);
 
+  const showLoadingScreen = gameState === 'loading';
+  const preload = gameState === 'preload';
+
   useEffect(() => {
-    if (gameState === 'loading') {
+    if (showLoadingScreen) {
       setShowSpinner(true);
 
       const timer = setTimeout(() => {
         setShowSpinner(false);
-        rankingCompleted === false ? setGameState('inProgress') : setGameState('finished');
+        !rankingCompleted ? setGameState('inProgress') : setGameState('finished');
       }, 3000);
 
       return () => clearTimeout(timer);
     }
   }, [gameState, setGameState, rankingCompleted]);
 
-  if (gameState === 'loading' || gameState === 'preload') {
+  if (showLoadingScreen || preload) {
     return (
       <div className="loading-container">
         {showSpinner && <div className="spinner"></div>}
