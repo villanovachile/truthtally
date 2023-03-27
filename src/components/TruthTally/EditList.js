0ae48a399a78c22e68e15c093f08f3488f48ffa4
Daniel R <daniel.r@automattic.com>
2023-03-26 22:52:24 -0700

    Refactor EditList component

--- src/components/TruthTally/EditList.js
+++ src/components/TruthTally/EditList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import Item from './Item';
 import AddItemForm from './AddItemForm';
